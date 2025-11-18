01d87ae763d1f1ebe35290d0f6f166f6539c8162
Arseniy Orlov <arseny.orlow@yandex.ru>
2025-11-18 04:38:34 +0000

    Add load deps plugin

---
+++ checker/plugins/load_deps.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+import pathlib
+import subprocess
+
+from checker.utils import print_info
+
+from .base import PluginABC, PluginOutput
+
+
+class LoadDepsPlugin(PluginABC):
+    name = "load_deps"
+
+    class Args(PluginABC.Args):
+        deps: list[str]
+        root: str
+        task_path: str
+
+    # type: ignore[override]
+    def _run(self, args: Args, *, verbose: bool = False) -> PluginOutput:
+        LoadDepsPlugin._load_deps(
+            deps=args.deps,
+            root=args.root,
+            task_path=args.task_path,
+            verbose=verbose,
+        )
+        return PluginOutput(output="Files have been copied")
+
+    @staticmethod
+    def _load_deps(
+        deps: list[str], root: str, task_path: str, verbose: bool = False
+    ) -> None:
+        remote = subprocess.run(
+            ["git", "config", "--get", "remote.origin.url"],
+            capture_output=True,
+            text=True,
+            check=True,
+            cwd=root
+        ).stdout.strip()
+        if verbose:
+            print_info(f"Remote URL: {remote}\n")
+
+        for dep in deps:
+            dep_path = pathlib.Path(task_path) / "_deps" / dep
+            if verbose:
+                print_info(f"Clone branch {dep} at {str(dep_path)}\n")
+
+            subprocess.run(
+                ["git", "clone", "--branch", dep, remote, str(dep_path)],
+                capture_output=True,
+                text=True,
+                check=True,
+                cwd=root
+            )
